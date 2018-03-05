1d081ef66e95b03a61b530f914c93b2672f4c84a
Lim Jiechao <jiechao.lim@gmail.com>
2018-03-05 23:08:34 +0800

    population of palette with colors on load
    Signed-off-by: Lim Jiechao <jiechao.lim@gmail.com>

--- src/Components/Palette.js
+++ src/Components/Palette.js
@@ -1,14 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
+import Color from './Color'
 import './Palette.css'
 
-class Palette extends Component {
-
-  render() {
+const Palette = ({ selectedTheme }) => {
+  const colors = Object.entries(selectedTheme).map(([index, color]) => {
     return (
-      <div>Palette</div>
+      <Color
+        colorIndex={index}
+        colorValue={color}
+        key={`color-${index}`}/>
     );
-  }
-
+  });
+  return (
+    <div id="palette">
+      { colors }
+    </div>
+  );
 }
 
 export default Palette;
