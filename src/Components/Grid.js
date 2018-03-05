22893980a456285ef511527c62eab72cf2504add
Lim Jiechao <jiechao.lim@gmail.com>
2018-03-06 00:50:24 +0800

    click handler function added
    Signed-off-by: Lim Jiechao <jiechao.lim@gmail.com>

--- src/Components/Grid.js
+++ src/Components/Grid.js
@@ -2,21 +2,22 @@ import React from 'react';
 import Cell from './Cell';
 import './Grid.css';
 
-const Grid = ({ selectedLength, selectedTheme, colors }) => {
+const Grid = ({ selectedLength, selectedTheme, colors, onCellClick }) => {
   const cells = [];
 
-  colors.forEach((color, cell) => {
+  colors.forEach((color, cell, onCellClick) => {
     cells.push(
       <Cell
         length={selectedLength}
         cell={cell}
-        color={selectedTheme[color]}
+        colorValue={selectedTheme[color]}
+        colorIndex={color}
         key={`cell-${cell}`} />
     );
   });
 
   return (
-    <div id="grid">
+    <div id="grid" onClick={onCellClick}>
       { cells }
     </div>
   );
