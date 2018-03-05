b542c33b2770c75917f5cf563c82577bd48bd875
Lim Jiechao <jiechao.lim@gmail.com>
2018-03-05 16:12:55 +0800

    bar with option and restart buttons and moves left
    Signed-off-by: Lim Jiechao <jiechao.lim@gmail.com>

---
+++ src/Components/Bar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Bar.css'
+
+class Bar extends Component {
+  render() {
+    return (
+      <div id="bar">
+        <img src="options.svg" id="options" alt="options button" className="button"/>
+        <span id="moves">30 / 30</span>
+        <img src="restart.svg" id="restart" alt="restart button" className="button"/>
+      </div>
+    );
+  }
+}
+
+export default Bar;
