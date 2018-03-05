128ce51347286d578f7cc213b09732a47f051759
Lim Jiechao <jiechao.lim@gmail.com>
2018-03-05 21:05:21 +0800

    component Picker renamed as Palette
    Signed-off-by: Lim Jiechao <jiechao.lim@gmail.com>

---
+++ src/Components/Palette.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Palette.css'
+
+class Palette extends Component {
+
+  render() {
+    return (
+      <div>Palette</div>
+    );
+  }
+
+}
+
+export default Palette;
