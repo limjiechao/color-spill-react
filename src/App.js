c61c58f33f907d7655631490e8510a06dde4ad3b
Lim Jiechao <jiechao.lim@gmail.com>
2018-03-05 16:13:23 +0800

    overall HTML structure
    Signed-off-by: Lim Jiechao <jiechao.lim@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import Title from './Components/Title'
+import Bar from './Components/Bar'
+import Grid from './Components/Grid'
+import Picker from './Components/Picker'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Color Spill!</h1>
+      <div id="app">
+        <Title />
+        <Bar />
+        <Grid />
+        <Picker />
       </div>
     );
   }
