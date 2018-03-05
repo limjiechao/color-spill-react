fc5849ed7d929c03eefeaba373b3c76167de5c74
Lim Jiechao <jiechao.lim@gmail.com>
2018-03-05 20:00:20 +0800

    default game props passed in
    Signed-off-by: Lim Jiechao <jiechao.lim@gmail.com>

--- src/index.js
+++ src/index.js
@@ -4,5 +4,10 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <App
+    selectedLength={2}
+    selectedDifficulty={1}
+    selectedTheme={1} />,
+  document.getElementById('root'));
 registerServiceWorker();
