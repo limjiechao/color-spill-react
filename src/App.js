d3bc2c5449cb76d52105deb5a3bdedcd80901626
Lim Jiechao <jiechao.lim@gmail.com>
2018-03-05 20:09:05 +0800

    state initialized and props to grid component
    Signed-off-by: Lim Jiechao <jiechao.lim@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
 import React, { Component } from 'react';
-import Title from './Components/Title'
-import Bar from './Components/Bar'
-import Grid from './Components/Grid'
-import Picker from './Components/Picker'
+// Game
+import Model from './Game/Model';
+import dimensions from './Game/Dimensions';
+import themes from './Game/themes';
+import difficulty from './Game/difficulty';
+// Components
+import Title from './Components/Title';
+import Bar from './Components/Bar';
+import Grid from './Components/Grid';
+import Picker from './Components/Picker';
+// Stylesheet
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedLength: props.selectedLength,
+      selectedDifficulty: props.selectedTheme,
+      selectedTheme: props.selectedDifficulty,
+      model: new Model(props.selectedLength),
+    }
+    console.log(this.state)
+  }
   render() {
     return (
       <div id="app">
         <Title />
         <Bar />
-        <Grid />
+        <Grid
+          selectedLength={this.state.model.length}
+          selectedTheme={themes.colors[this.state.selectedTheme]}
+          colors={this.state.model.colors} />
         <Picker />
       </div>
     );
