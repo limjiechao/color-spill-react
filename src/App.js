aa36cd73d24904d9e253b5a720cdd10c0938d3ab
Lim Jiechao <jiechao.lim@gmail.com>
2018-03-06 22:29:27 +0800

    moves counter, refactored direct state mutation
    Signed-off-by: Lim Jiechao <jiechao.lim@gmail.com>

--- src/App.js
+++ src/App.js
@@ -15,42 +15,81 @@ import './Stylesheets/App.css';
 class App extends Component {
   constructor(props) {
     super(props);
+
+    let { length, level, theme } = props
+
+    let moves = difficulty.level[level][length];
+    let model = new Model(length);
+    let oldFillColor = model.getOldFillColor();
+
     this.state = {
-      length: props.length,
-      difficulty: props.difficulty,
-      theme: props.theme,
-      model: new Model(props.length),
-      moves: difficulty.level[props.difficulty][props.length]
+      length,
+      level,
+      theme,
+      model,
+      totalMoves: moves,
+      remainingMoves: moves,
+      oldFillColor
     }
+
     this.onClick = this.onClick.bind(this);
     this.fill = this.fill.bind(this);
+    this.deductMoves = this.deductMoves.bind(this);
   }
 
   fill(color) {
-    this.state.model.fill(color);
+    let { model } = this.state;
+    model.fill(color);
+    this.setState({
+      model
+    })
+  }
+
+  deductMoves() {
+    let { remainingMoves } = this.state;
+    remainingMoves -= 1;
+    this.setState({
+      remainingMoves
+    });
+  }
+
+  setOldFillColor(oldFillColor) {
+    this.setState({
+      oldFillColor
+    });
   }
 
   onClick(event) {
     let target = event.target.attributes.color
+    let { remainingMoves, oldFillColor } = this.state;
+
     if (target !== undefined) {
       let colorClicked = Number(target.value);
-      this.fill(colorClicked);
-      this.setState({});
+      
+      if (colorClicked !== oldFillColor) {
+        this.deductMoves();
+        this.fill(colorClicked);
+        this.setOldFillColor(colorClicked);
+      }
     }
   }
 
   render() {
+    let { remainingMoves, totalMoves, model, theme } = this.state;
+
     return (
       <div id="app">
         <Title />
-        <Bar />
+        <Bar
+          remainingMoves={remainingMoves}
+          totalMoves={totalMoves} />
         <Grid
-          length={this.state.model.length}
-          theme={themes.colors[this.state.theme]}
-          colors={this.state.model.colors}
+          length={model.length}
+          theme={themes.colors[theme]}
+          colors={model.colors}
           onClick={this.onClick} />
         <Palette
-          theme={themes.colors[this.state.theme]}
+          theme={themes.colors[theme]}
           onClick={this.onClick} />
       </div>
     );
