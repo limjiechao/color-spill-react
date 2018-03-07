8ea31c276cf816013acd9066dfa08c5da7b08df8
Lim Jiechao <jiechao.lim@gmail.com>
2018-03-07 23:54:36 +0800

    title now shows victory or defeat message
    Signed-off-by: Lim Jiechao <jiechao.lim@gmail.com>

--- src/Components/Title.js
+++ src/Components/Title.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import '../Stylesheets/Title.css';
 
-const Title = () => {
+const Title = ({ text }) => {
+  text = text.replace(/ /g, "\u00a0");
+
   return (
-    <h1 id="title">COLOR&nbsp;SPILL!</h1>
+    <h1 id="title">
+      { text }
+    </h1>
   );
 }
 
