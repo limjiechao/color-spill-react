3944bca7673e4d02d3578760382c8d9de7d810b9
Lim Jiechao <jiechao.lim@gmail.com>
2018-03-07 23:55:01 +0800

    restart button fully working
    Signed-off-by: Lim Jiechao <jiechao.lim@gmail.com>

--- src/Components/Bar.js
+++ src/Components/Bar.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import '../Stylesheets/Bar.css'
 
-const Bar = ({ remainingMoves, totalMoves }) => {
+const Bar = ({ remainingMoves, totalMoves, restartGame }) => {
   return (
     <div id="bar">
-      <img src="options.svg" id="options" alt="options button" className="button"/>
+      <img
+        src="options.svg"
+        id="options"
+        alt="options button"
+        className="button" />
       <span id="moves">
         { remainingMoves } / { totalMoves }
       </span>
-      <img src="restart.svg" id="restart" alt="restart button" className="button"/>
+      <img
+        onClick={restartGame}
+        src="restart.svg"
+        id="restart"
+        alt="restart button"
+        className="button" />
     </div>
   );
 }
